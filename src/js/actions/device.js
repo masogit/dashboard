edb2b0e7e7e268865a1109ed734edbdda121bf61
Man, Shuo <shuo.man@hpe.com>
2016-11-01 23:25:21 +0800

    Use Rest connect reomte, failed by Access-Control-Allow-Origin

--- src/js/actions/device.js
+++ src/js/actions/device.js
@@ -1,6 +1,14 @@
 import { types } from '../reducers';
+import { Rest } from 'grommet';
 
+const url_deviceTypes = "http://www.zhiyuninfo.com:8080/devicemanager-1.0/dmrest/devicetypes";
 export function loadDeviceTypes() {
+  Rest.default.setHeader('Access-Control-Allow-Origin', 'http://www.zhiyuninfo.com');
+  Rest.default.get(url_deviceTypes).then((data) => {
+    console.log(data);
+  }, (reason) => {
+    console.log(reason);
+  });
   let deviceTypes = ['燃烧器', '锅炉', '天气检测'];
   return {
     type: types.INIT_DEVICE_TYPE,
