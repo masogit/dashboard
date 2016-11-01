0b319a8afbff8d25981980129dec3b1b9bb6fc7f
Man, Shuo <shuo.man@hpe.com>
2016-11-02 01:19:27 +0800

    User http proxy get remote data

--- server/index.js
+++ server/index.js
@@ -1,12 +1,21 @@
 var express = require('express');
-var path = require('path');
+var http = require('http-proxy');
+// var path = require('path');
 var app = express();
+var router = express.Router();
+var proxy = http.createProxyServer();
 
+app.use(router);
 
-app.get('/*', function(req, res) {
-  console.log('----');
-  res.sendFile(path.resolve(path.join(__dirname, '/../dist/index.html')));
+// http://www.zhiyuninfo.com:8080/devicemanager-1.0/dmrest/devicetypes
+app.use('/devicemanager', (req, res) => {
+  proxy.web(req, res, {target: "http://www.zhiyuninfo.com:8080/devicemanager-1.0/dmrest"});
 });
+
+// router.get('/*', function(req, res) {
+//   console.log('----');
+//   res.sendFile(path.resolve(path.join(__dirname, '/../dist/index.html')));
+// });
 
 app.listen(9000, function () {
   console.log('Example app listening on port 9000!');
