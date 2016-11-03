346c0df67b816f47ce376531a4bac003c6277c15
Man, Shuo <shuo.man@hpe.com>
2016-11-04 02:50:37 +0800

    Add Legend

--- src/js/containers/Device.js
+++ src/js/containers/Device.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { Box, Tabs, Tab } from 'grommet';
+import { Box, Tabs, Tab, Sidebar, Legend, Heading } from 'grommet';
 import { Map, MarkerGroup } from 'react-d3-map';
 import { Table } from '../components';
 import { deviceActions } from '../actions';
 
 const mapConf = {
-  width: 1400,
+  width: 1200,
   height: 600,
   scale: 4000,
   scaleExtent: [1 << 12, 1 << 13],
-  center: [111, 36]
+  center: [100, 36]
 };
 
 class Device extends Component {
@@ -20,30 +20,34 @@ class Device extends Component {
   }
 
   renderMarkerGroup(records) {
-    return records.map((record) => {
-      var data = {
-        "type": "Feature",
-        "properties": {
-          "text": record.name
-        },
-        "geometry": {
-          "type": "Point",
-          "coordinates": [record.longitude, record.latitude]
-        }
-      };
+    return records.map((data) => {
       return <MarkerGroup data={data}/>;
     });
   }
 
+  renderLegends(legends) {
+    return Object.keys(legends).map((key) => {
+      return (
+        <Box pad="small">
+          <Heading uppercase={true} tag="h3">{key}</Heading>
+          <Legend series={legends[key]} total={true} />
+        </Box>
+      );
+    });
+  }
+
   render() {
-    const { records } = this.props;
+    const { records, markers, legends } = this.props;
     return (
       <Box pad="small">
         <Tabs>
           <Tab title="Location">
-            <Box size="large">
+            <Box direction="row">
+              <Sidebar full={false}>
+                { this.renderLegends(legends) }
+              </Sidebar>
               <Map {...mapConf}>
-                { this.renderMarkerGroup(records) }
+                { this.renderMarkerGroup(markers) }
               </Map>
             </Box>
             <Table data={records}/>
@@ -58,7 +62,9 @@ class Device extends Component {
 
 let mapStateToProps = (state) => {
   return {
-    records: state.device.records
+    records: state.device.records,
+    markers: state.device.markers,
+    legends: state.device.legends
   };
 };
 
