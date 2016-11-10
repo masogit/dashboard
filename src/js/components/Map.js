c90721a670ce1bc7fc01a03b9b39c02feb89d2e6
Huang Ling <lingling374@qq.com>
2016-11-11 00:20:15 +0800

    refactor point

--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -15,6 +15,8 @@ export default class Map extends Component {
       points: null,
       showLayer: false
     };
+    this.mouseenter = this.mouseenter.bind(this);
+    this.mouseout = this.mouseout.bind(this);
   }
   componentDidMount() {
     const width = 950, height = 680;
@@ -43,6 +45,15 @@ export default class Map extends Component {
       this.setState({
         features,
         points
+      }, () => {
+        this.renderMap();
+        if (this.props.showPoints) {
+          this.renderPoints();
+        }
+
+        if (this.props.showLabel) {
+          //this.renderLabel();
+        }
       });
     });
   }
@@ -57,15 +68,7 @@ export default class Map extends Component {
         .enter().append("path")
         .attr("class", 'subunit')
         .attr("d", path)
-        .style("fill", (d, a) => d.properties.color)
-        .on('click', (d, a) => {
-          const layer = this.layer.boxContainerRef;
-          layer.style.left = d3.event.pageX + "px";
-          layer.style.top = (d3.event.pageY - 60) + "px";
-
-          this.setState({ showLayer: true }/*, () =>
-            setTimeout(() => this.setState({ showLayer: false }), 3000)*/);
-        });
+        .style("fill", (d, a) => d.properties.color);
         
       const events = this.props.events;
       if (events) {
@@ -74,6 +77,21 @@ export default class Map extends Component {
         });
       }
     }
+  }
+
+  mouseenter(d, i) {
+    this.mapContainer.selectAll(".subunit")[0][i].style.opacity = 0.9;
+    const layer = this.layer.boxContainerRef;
+    layer.style.left = d3.event.pageX + "px";
+    layer.style.top = (d3.event.pageY - 60) + "px";
+
+    this.setState({ showLayer: true }/*, () =>
+      setTimeout(() => this.setState({ showLayer: false }), 3000)*/);
+  }
+
+  mouseout(d, i) {
+    this.mapContainer.selectAll(".subunit")[0][i].style.opacity = 1;
+    this.setState({ showLayer: false })
   }
 
   renderPoints() {  
@@ -85,26 +103,14 @@ export default class Map extends Component {
       const {mapContainer, path} = this;
       const circleNumber = 3;
       for(let i = 0; i < circleNumber; i++) {
-         mapContainer.selectAll('point' + i)
+         mapContainer.selectAll('point')
           .data(this.state.points)
           .enter().append("circle")
           .attr('class', 'circle')
           .attr("transform", function (d) { return "translate(" + path.centroid(d) + ")"; })
           .attr("r", function (d) { return radius(d.number) * (1 + i/4) })
-          .on('click', (d, a) => {
-            const layer = this.layer.boxContainerRef;
-            layer.style.left = d3.event.pageX + "px";
-            layer.style.top = (d3.event.pageY - 60) + "px";
-
-            this.setState({ showLayer: true }/*, () =>
-              setTimeout(() => this.setState({ showLayer: false }), 3000)*/);
-          })
-          .on('mouseenter', (d, i)=> {
-            mapContainer.selectAll(".subunit")[0][i].style.opacity = 0.9;
-          })
-          .on('mouseout', (d, i)=> {
-            mapContainer.selectAll(".subunit")[0][i].style.opacity = 1;
-          });
+          .on('mouseenter', this.mouseenter)
+          .on('mouseout', this.mouseout);
       }
     }
   }
@@ -134,14 +140,6 @@ export default class Map extends Component {
 
   render() {
     const {showLabel, showPoints, events, children} = this.props;
-    this.renderMap();
-    if (showPoints) {
-      this.renderPoints();
-    }
-
-    if (showLabel) {
-      //this.renderLabel();
-    }
     return (
       <Box className='map'>
         <div id='map' />
