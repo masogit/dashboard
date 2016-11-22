2a243ff203fcfc2b5379b41193a1aec92d91a470
Man, Shuo <shuo.man@gmail.com>
2016-11-22 21:06:44 +0800

    Change Siderbar menus

--- src/js/reducers/header.js
+++ src/js/reducers/header.js
@@ -1,79 +1,66 @@
 import { TYPE } from '../constants';
 
 const initialState = {
-  logo: 'shortcut-icon.png',
-  title: 'Dashboard Platform',
-  showSidebar: true,
-  menus: [
-    { root: 'DASHBOARD' },
-    {
-      title: 'Decks', icon: 'dashboard', active: true, menus: [
-        { title: 'Group1', router: '/preview', icon: 'circle' },
-        {
-          title: 'Group2', router: '/dashboard2', icon: 'circle', active: true,
-          menus: [
-            { title: 'd2', router: '/home', active: true },
-            { title: 'd3', router: '/deck' }
-          ]
-        }
-      ]
-    },
-    { title: 'Widgets', router: '/home', icon: 'heartbeat', status: [{ text: 4, color: 'blue' }] },
-    { title: 'Source Data', router: '/device/history', icon: 'history', status: [{ text: 'new', color: 'green' }] },
-    {
-      title: 'Actions', router: '/device/alarm', icon: 'warning', menus: [
-        { title: 'a1', router: '/dashboard1' },
-        { title: 'a2', router: '/dashboard2' }
-      ]
-    },
-    { title: 'Mailbox', router: '/device/alarm', icon: 'envelope', status: [{ text: '5', color: 'red' }, { text: '3', color: 'green' }, { text: 4, color: 'yellow' }] },
-    { root: 'DEVICE' },
-    { title: 'Overview', router: '/device', icon: 'circle-o text-green' },
-    { title: 'Status', router: '/device/status', icon: 'circle-o text-red' },
-    { title: 'History', router: '/device/history', icon: 'circle-o text-yellow' },
-    { title: 'Alarm', router: '/device/alarm', icon: 'circle-o text-blue' },
-    { root: 'INCIDENT' },
-    { title: 'Overview', router: '/device', icon: 'circle-o text-green' },
-    { title: 'Status', router: '/device/status', icon: 'circle-o text-red' },
-    { title: 'History', router: '/device/history', icon: 'circle-o text-yellow' },
-    { title: 'Alarm', router: '/device/alarm', icon: 'circle-o text-blue' }
+    logo: 'shortcut-icon.png',
+    title: 'Dashboard Platform',
+    showSidebar: true,
+    menus: [
 
-  ],
-  modules: [
-    { title: 'LIBRARY', router: '/home', icon: 'envelope', number: '4' },
-    { title: 'COMPOSE', router: '/deck', icon: 'bell', number: '10', status: 'warning' },
-    { title: 'PERFORM', router: '/preview', icon: 'flag', number: '9', status: 'danger' }
-  ],
-  user: {
-    name: 'Admin',
-    email: 'admin@abc.com'
-  }
+        { root: 'DASHBOARD' },
+        { title: 'HOME', icon: 'dashboard', active: true, router: "/home" },
+        { title: 'Widgets', router: '/home', icon: 'heartbeat', status: [{ text: 4, color: 'blue' }] },
+        { title: 'Designer', router: '/deck', icon: 'history', status: [{ text: 'new', color: 'green' }] },
+        { title: 'Fullscreen', router: '/preview', icon: 'warning' },
+        { title: 'Library', router: '/device/alarm', icon: 'envelope', status: [{ text: '5', color: 'red' }, { text: '3', color: 'green' }, { text: 4, color: 'yellow' }] },
+
+        { root: 'DEVICE' },
+        { title: 'Overview', router: '/device', icon: 'circle-o text-green' },
+        { title: 'Status', router: '/device/status', icon: 'circle-o text-red' },
+        { title: 'History', router: '/device/history', icon: 'circle-o text-yellow' },
+        { title: 'Alarm', router: '/device/alarm', icon: 'circle-o text-blue' },
+
+        { root: 'INCIDENT' },
+        { title: 'Overview', router: '/device', icon: 'circle-o text-green' },
+        { title: 'Status', router: '/device/status', icon: 'circle-o text-red' },
+        { title: 'History', router: '/device/history', icon: 'circle-o text-yellow' },
+        { title: 'Alarm', router: '/device/alarm', icon: 'circle-o text-blue' }
+
+    ],
+    modules: [
+        { title: 'LIBRARY', router: '/home', icon: 'envelope', number: '4' },
+        { title: 'COMPOSE', router: '/deck', icon: 'bell', number: '10', status: 'warning' },
+        { title: 'PERFORM', router: '/preview', icon: 'flag', number: '9', status: 'danger' }
+    ],
+    user: {
+        name: 'Admin',
+        email: 'admin@abc.com'
+    }
 };
 
 const handlers = {
-  [TYPE.LOGIN]: (state, action) => ({ user: { name: action.login } }),
-  [TYPE.INIT_DEVICE_TYPE]: (state, action) => {
-    let deviceTypes = action.deviceTypesMenus;
-    let modules = state.modules;
-    let menuDevice = modules.filter((module) => {
-      return module.title === 'SETTINGS';
-    })[0].menus.Device;
+    [TYPE.LOGIN]: (state, action) => ({ user: { name: action.login } }),
+    [TYPE.INIT_DEVICE_TYPE]: (state, action) => {
+        let deviceTypes = action.deviceTypesMenus;
+        let modules = state.modules;
+        let menuDevice = modules.filter((module) => {
+            return module.title === 'SETTINGS';
+        })[0].menus.Device;
 
-    deviceTypes.forEach((type) => {
-      menuDevice.push({
-        title: type, router: '/settings/device/' + type
-      });
-    });
+        deviceTypes.forEach((type) => {
+            menuDevice.push({
+                title: type, router: '/settings/device/' + type
+            });
+        });
 
-    return { 'modules': modules };
-  },
-  [TYPE.TOGGLE_SIDEBAR]: state => {
-    return { showSidebar: !state.showSidebar };
-  }
+        return { 'modules': modules };
+    },
+    [TYPE.TOGGLE_SIDEBAR]: state => {
+        return { showSidebar: !state.showSidebar };
+    }
 };
 
-export default function (state = initialState, action) {
-  let handler = handlers[action.type];
-  if (!handler) return state;
-  return {...state, ...handler(state, action) };
+export default function(state = initialState, action) {
+    let handler = handlers[action.type];
+    if (!handler) return state;
+    return {...state, ...handler(state, action) };
 };
