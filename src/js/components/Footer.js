03155b57e8661ab59ed8aa0ad6876892c050f495
Man, Shuo <shuo.man@gmail.com>
2016-11-29 00:18:34 +0800

    Update Footer

--- src/js/components/Footer.js
+++ src/js/components/Footer.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import { Footer } from 'grommet';
+import { Footer, Box } from 'grommet';
 
 export default class FooterArea extends Component {
   render() {
     return (
-        <Footer separator='top'>
-            This is Footer
-        </Footer>
+      <Footer separator='top' direction="row" justify="between" pad={{ horizontal: 'small' }}>
+        <Box><strong>Copyright © 2016-2017 <a href="http://masogit.github.io">Maso Studio</a>.</strong></Box>
+        <Box direction="row"><strong>Version - </strong> 0.0.1</Box>
+      </Footer>
     );
   }
 }
